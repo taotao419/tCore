f27276b6545dfd7a7aa09e8a36f797a2c6d3c5a7
Taotao419 <zkl_369@msn.com>
2022-11-24 13:03:11 +0800

    finish chapter 1

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -1,5 +1,6 @@
-use crate::{sbi::shutdown, println};
+use crate::{sbi::shutdown};
 use core::panic::PanicInfo;
+
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,9 @@ global_asm!(include_str!("entry.asm"));
 #[no_mangle]
 pub fn rust_main()->!{
     clear_bss();
-    println!("Hello world!");
+    info("Hello world !");
+    warn("Hello world !");
+    error("Hello world !");
     println!("你好世界");
     panic!("Shutdown machine!");
 }
@@ -21,10 +23,34 @@ pub fn rust_main()->!{
 /// clear BSS segment
 pub fn clear_bss() {
     extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
         fn sbss();
         fn ebss();
     }
     (sbss as usize..ebss as usize).for_each(|a| 
         unsafe { (a as *mut u8).write_volatile(0) 
     });
+
+    println!("\x1b[34m[INFO] text range : [{:#x},{:#x}) \x1b[0m ",stext as usize, etext as usize);
+    println!("\x1b[34m[INFO] rodata range : [{:#x},{:#x}) \x1b[0m ",srodata as usize, erodata as usize);
+    println!("\x1b[34m[INFO] data range : [{:#x},{:#x}) \x1b[0m ",sdata as usize, edata as usize);
+    println!("\x1b[34m[INFO] bss range : [{:#x},{:#x}) \x1b[0m ",sbss as usize, ebss as usize);
+    
+}
+
+pub fn info(s:&str) {
+    println!("\x1b[34m[INFO] {}\x1b[0m",s); 
+}
+
+pub fn warn(s:&str) {
+    println!("\x1b[93m[WARN] {}\x1b[0m",s); 
+}
+
+pub fn error(s:&str) {
+    println!("\x1b[31m[ERROR] {}\x1b[0m",s); 
 }
