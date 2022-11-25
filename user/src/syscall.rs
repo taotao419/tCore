bec33842fc331a963ee996f7cf52bfb0c2b2d708
Taotao419 <zkl_369@msn.com>
2022-11-25 14:01:11 +0800

    ch2 finish user applications.

---
+++ user/src/syscall.rs
@@ -0,0 +1,33 @@
+use core::arch::asm;
+
+const SYSCALL_WRITE: usize = 64;
+const SYSCALL_EXIT: usize = 93;
+
+
+// _start: addi  a0, x0, 1      # 1 = StdOut
+//         la    a1, helloworld # load address of helloworld
+//         addi  a2, x0, 13     # length of our string
+//         addi  a7, x0, 64     # linux write system call
+//         ecall                # Call linux to output the string
+
+fn syscall(id:usize,args:[usize;3])->isize{
+    let mut ret:isize;
+    unsafe{
+        asm!(
+            "ecall",
+            inlateout("x10") args[0] => ret,  //in and late out
+            in("x11") args[1],
+            in("x12") args[2],
+            in("x17") id
+        );
+    }
+    ret
+}
+
+pub fn sys_write(fd:usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE,[fd,buffer.as_ptr() as usize,buffer.len()])
+}
+
+pub fn sys_exit(exit_code: i32) -> isize {
+    syscall(SYSCALL_EXIT, [exit_code as usize, 0, 0])
+}
