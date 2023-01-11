e2eaff054afb62b885fca383617006d5601cb978
Carrier Zhang <carrier.zhang@agfa.com>
2023-01-11 13:40:52 +0800

    ch5 : init/fork/exec + initproc and user shell

---
+++ user/src/bin/initproc.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate user_lib;
+
+use user_lib::{exec, fork, yield_,wait};
+
+#[no_mangle]
+fn main() -> i32 {
+    if fork() == 0 {
+        //此时 fork()之后, 已经分裂为独立的两个进程了, 这部分是子进程开始运行user_shell这个User APP
+        exec("user_shell\0");
+    } else {
+        //这里还是父进程 或者说原进程. 在原进程的代码执行中永远会执行else块中的代码
+        loop {
+            let mut exit_code: i32 = 0;
+            let pid = wait(&mut exit_code);
+            //等待有没有准备销毁的子进程 作为所有用户进程的爷爷(祖宗) 时时刻刻准备回收子进程的资源
+            if pid == -1 {
+                //pid 如果是-1 说明没有子进程准备销毁. 那么就交还CPU控制权
+                yield_();
+                continue;
+            }
+            //是有子进程销毁的
+            println!(
+                "[initproc] Released a zombie process, pid={}, exit_code={}",
+                pid, exit_code
+            );
+        }
+    }
+
+    return 0;
+}
---
+++ user/src/bin/user_shell.rs
@@ -0,0 +1,66 @@
+#![no_std]
+#![no_main]
+#![allow(clippy::println_empty_string)]
+
+use alloc::string::String;
+use user_lib::console::getchar;
+use user_lib::{exec, fork,waitpid};
+
+extern crate alloc;
+
+#[macro_use]
+extern crate user_lib;
+
+const LF: u8 = 0x0au8; //回车
+const CR: u8 = 0x0du8; //回车
+const DL: u8 = 0x7fu8; //DELETE
+const BS: u8 = 0x08u8; //Back Space
+
+#[no_mangle]
+pub fn main() -> i32 {
+    println!("Rust user shell");
+    let mut line: String = String::new();
+    print!("&>> ");
+    loop {
+        let c = getchar();
+        match c {
+            LF | CR => {
+                //判断回车之后
+                println!("");
+                if !line.is_empty() {
+                    line.push('\0');
+                    let pid = fork();
+                    if pid == 0 {
+                        //child process
+                        if exec(line.as_str()) == -1 {
+                            println!("Error when executing!");
+                            return -4;
+                        }
+                        panic!("Unreachable in rust_main!");
+                    } else {
+                        //original process
+                        let mut exit_code: i32 = 0;
+                        //这个是loop的wait, 很有可能子进程exec不成功, 它的进程需要父进程立即回收掉.
+                        let exit_pid = waitpid(pid as usize, &mut exit_code);
+                        assert_eq!(pid, exit_pid);
+                        println!("Shell: Process {} exited with code {}", pid, exit_code);
+                    }
+                    line.clear();//回车意味着提示符清空
+                }
+                print!("&>>");
+            }
+            BS | DL => { //删除 退格键
+                if !line.is_empty() {
+                    print!("{}", BS as char); //why twice?
+                    print!(" ");
+                    print!("{}", BS as char);
+                    line.pop();
+                }
+            }
+            _ => { //默认情况就是提示符打印输入的字符
+                print!("{}", c as char);
+                line.push(c as char);
+            }
+        }
+    }
+}
