5a71e3ffe2f00b8a5d7c524d24c334897fac4e8b
zkl <zhangkanli@zhangkanlideiMac.local>
2023-02-18 08:28:20 +0800

    ch6 : cargo new easy-fs-fuse for testing

---
+++ easy-fs-fuse/src/main.rs
@@ -0,0 +1,28 @@
+use easy_fs::{BlockDevice, EasyFileSystem};
+use std::io::{Read, Seek, SeekFrom, Write};
+use std::fs::{read_dir, File, OpenOptions};
+use std::sync::Mutex;
+
+const BLOCK_SZ: usize = 512;
+
+struct BlockFile(Mutex<File>);
+
+impl BlockDevice for BlockFile {
+    fn read_block(&self, block_id: usize, buf: &mut [u8]) {
+        let mut file = self.0.lock().unwrap();
+        file.seek(SeekFrom::Start((block_id * BLOCK_SZ) as u64))
+            .expect("Error when seeking!");
+        assert_eq!(file.read(buf).unwrap(), BLOCK_SZ, "Not a complete block!");
+    }
+
+    fn write_block(&self, block_id: usize, buf: &[u8]) {
+        let mut file = self.0.lock().unwrap();
+        file.seek(SeekFrom::Start((block_id * BLOCK_SZ) as u64))
+            .expect("Error when seeking!");
+        assert_eq!(file.write(buf).unwrap(),BLOCK_SZ,"Not a complete block!");
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
